2161e4663a9ac55c655c79b505b267162cdb96aa
Baertram <pz1@gmx.net>
2025-02-06 17:23:59 +0100

    FCOCS v2.9 -Added UI settings for Golden Pursuits: Hide tracker

--- FCOChangeStuff/src/FCOCS_UI.lua
+++ FCOChangeStuff/src/FCOCS_UI.lua
@@ -2,28 +2,37 @@ if FCOCS == nil then FCOCS = {} end
 local FCOChangeStuff = FCOCS
 
 
-local origPromotionalEventTrackerUpdate
-local origPromotionalEventTrackerUpdateOverwritten = false
+local preventFCOCSPostHook                    = false
+local origPromotionalEventTrackerUpdateHooked = false
 
 local function updatePromotionalEventTrackerVisibilityState(doHide)
+    if doHide == nil then
+        preventFCOCSPostHook = true
+        --Call updater routine to show or hide accordningly but do not use the FCOCS SecurePostHook then!
+        PROMOTIONAL_EVENT_TRACKER:Update()
+        preventFCOCSPostHook = false
+        return
+    end
     PROMOTIONAL_EVENT_TRACKER:GetFragment():SetHiddenForReason("NoTrackedPromotionalEvent", doHide, DEFAULT_HUD_DURATION, DEFAULT_HUD_DURATION)
 end
 
 function FCOChangeStuff.PromotionalEventTrackerUIChanges(hideNow)
     if PROMOTIONAL_EVENT_TRACKER == nil then return end
-    origPromotionalEventTrackerUpdate = origPromotionalEventTrackerUpdate or PROMOTIONAL_EVENT_TRACKER.Update
 
-    if not FCOChangeStuff.settingsVars.settings.hidePromotionalEventTracker then return end
+    if not hideNow and not FCOChangeStuff.settingsVars.settings.hidePromotionalEventTracker then
+        updatePromotionalEventTrackerVisibilityState(nil)
+        return
+    end
 
-    if not origPromotionalEventTrackerUpdateOverwritten then
-        function PROMOTIONAL_EVENT_TRACKER.Update()
+    if not origPromotionalEventTrackerUpdateHooked then
+        SecurePostHook(PROMOTIONAL_EVENT_TRACKER, "Update", function(...)
+            if preventFCOCSPostHook then return end
+
             if FCOChangeStuff.settingsVars.settings.hidePromotionalEventTracker == true then
                 updatePromotionalEventTrackerVisibilityState(true) -- Always hide
-            else
-                return origPromotionalEventTrackerUpdate()
             end
-        end
-        origPromotionalEventTrackerUpdateOverwritten = true
+        end)
+        origPromotionalEventTrackerUpdateHooked = true
     end
 
     if hideNow == true then
