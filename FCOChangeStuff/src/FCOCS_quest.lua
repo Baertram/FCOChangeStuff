a865621e741720d14171cee61631cba0f9ff9c4d
Baertram <pz1@gmx.net>
2025-02-23 22:53:53 +0100

    -Added setting for movable tracker quest UI, including saved position -Added settings to control "player spinning" individually for inventory, collections, character, and others Works with the "Prevent stop interaction if menu opened" too

--- FCOChangeStuff/src/FCOCS_quest.lua
+++ FCOChangeStuff/src/FCOCS_quest.lua
@@ -4,41 +4,73 @@ local FCOChangeStuff = FCOCS
 ------------------------------------------------------------------------------------------------------------------------
 -- Quest --
 ------------------------------------------------------------------------------------------------------------------------
-local questTracker = ZO_FocusedQuestTrackerPanel
+local questTrackerHeader1 = nil --ZO_FocusedQuestTrackerPanelContainerQuestContainerTrackedHeader1
 local questTrackerOnMoveHooked = false
 
 function FCOChangeStuff.QuestTrackerLoadPosition()
-    local questTrackerSavedPosition = FCOChangeStuff.settingsVars.settings.questTrackerPos
-    if questTrackerSavedPosition.x > -1  and questTrackerSavedPosition.y > -1 then
-        questTracker:ClearAnchors()
-        questTracker:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, questTrackerSavedPosition.x, questTrackerSavedPosition.y)
+    if questTrackerHeader1 == nil then return end
+
+    local settings = FCOChangeStuff.settingsVars.settings
+    if settings.questTrackerMovable then
+        local questTrackerSavedPosition = settings.questTrackerPos
+        if questTrackerSavedPosition.x > -1  and questTrackerSavedPosition.y > -1 then
+            questTrackerHeader1:ClearAnchors()
+            questTrackerHeader1:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, questTrackerSavedPosition.x, questTrackerSavedPosition.y)
+        end
     end
 end
 
-function FCOChangeStuff.QuestTrackerMovable(isMovable)
+function FCOChangeStuff.QuestTrackerMovable(isMovable, loadPos)
     isMovable = isMovable or false
-    questTracker:SetMovable(isMovable)
+    questTrackerHeader1:SetMouseEnabled(isMovable)
+    questTrackerHeader1:SetMovable(isMovable)
 
     if not questTrackerOnMoveHooked then
-        ZO_PostHookHandler(questTracker, "OnMoveStop", function(questTrackerCtrl)
+        local doNotSaveMovedPos = false
+        questTrackerHeader1:SetHandler("OnMoveStop", function(questTrackerCtrl)
+--d("[FCOCS]questTrackerHeader1:OnMoveStop")
+            --if not isMoving then return end
+            if doNotSaveMovedPos then
+                doNotSaveMovedPos = false
+                return
+            end
             local settings = FCOChangeStuff.settingsVars.settings
-            settings.questTrackerPos.x =  questTrackerCtrl:GetLeft()
-            settings.questTrackerPos.y =  questTrackerCtrl:GetTop()
+            if settings.questTrackerMovable then
+                settings.questTrackerPos.x =  questTrackerCtrl:GetLeft()
+                settings.questTrackerPos.y =  questTrackerCtrl:GetTop()
+            end
         end)
+
+        ZO_PreHook(FOCUSED_QUEST_TRACKER, "AssistNext", function()
+            doNotSaveMovedPos = true
+        end)
+
+        --Set callback function for quest tracker updated (e.g. if AssistNext function was called) and load the saved position again
+        CALLBACK_MANAGER:RegisterCallback("QuestTrackerUpdatedOnScreen", function()
+--d("[FCOCS]CALLBACK_MANAGER fired QuestTrackerUpdatedOnScreen")
+            doNotSaveMovedPos = false
+            if FCOChangeStuff.settingsVars.settings.questTrackerMovable then
+                FCOChangeStuff.QuestTrackerLoadPosition()
+            end
+        end)
+
         questTrackerOnMoveHooked = true
     end
 
+    if loadPos == true then
+        FCOChangeStuff.QuestTrackerLoadPosition()
+    end
 end
 
-
 function FCOChangeStuff.QuestTrackerChanges()
-    local settings = FCOChangeStuff.settingsVars.settings
-    FCOChangeStuff.QuestTrackerMovable(settings.questTrackerMovable)
+--d("[FCOCS]QuestTrackerChanges")
+    FCOChangeStuff.QuestTrackerMovable(FCOChangeStuff.settingsVars.settings.questTrackerMovable)
 end
 
 ------------------------------------------------------------------------------------------------------------------------
 --Enable the quest modifications
 function FCOChangeStuff.questChanges()
+    questTrackerHeader1 = ZO_FocusedQuestTrackerPanelContainerQuestContainerTrackedHeader1
     FCOChangeStuff.QuestTrackerChanges()
     FCOChangeStuff.QuestTrackerLoadPosition()
 end
